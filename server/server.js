fa7de96d0f09b8f5af243eaaa6053101a6602c2d
Tweakings <tweakingsdev@gmail.com>
2018-03-14 20:00:59 -0700

    Added sockets with basic connection detection

--- server/server.js
+++ server/server.js
@@ -2,12 +2,26 @@ require('./config');
 
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
 var publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, () => {
+io.on('connection', (socket) => {
+  console.log('User connected to socket');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected from server');
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log('Server started on port ' + process.env.PORT);
 });
