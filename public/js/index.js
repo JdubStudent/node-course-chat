53b8a92b8fe98056344364b6cd90925481b6e24a
Tweakings <tweakingsdev@gmail.com>
2018-03-16 13:22:48 -0700

    Added messages css, Mustache templates, and error handly

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 var socket = io();
 
+function scrollToBottom() {
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //heights
+  var messagesHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  var offsetPadding = 20;
+  messagesHeight += newMessageHeight+lastMessageHeight;
+
+  if (messagesHeight + scrollTop + offsetPadding >= scrollHeight) {
+    // console.log('Should scroll messages');
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', () => {
   console.log('Connected to server');
   // socket.emit('sendMessage', {
@@ -12,39 +32,72 @@ socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
 
+var username = jQuery('[name=username]');
+
 socket.on('newMessage', (message) => {
   console.log('New Message', message);
 
   var formattedt = moment(message.sendDate).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.sender} ${formattedt}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  if (username.val() === message.sender) message.sender = 'Me';
+
+  var html = Mustache.render(template, {
+    sender: message.sender,
+    sendDate: formattedt,
+    text: message.text
+  });
+
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.sender} ${formattedt}: ${message.text}`);
   
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newGeolocationMessage', (message) => {
   var formattedt = moment(message.sendDate).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My Current Location</a>');
+  if (username.val() === message.sender) message.sender = 'Me';
 
-  li.text(`${message.sender} ${formattedt}: `);
-  a.attr('href', message.url);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    sender: message.sender,
+    sendDate: formattedt,
+    url: message.url
+  });
 
-  li.append(a);
-  jQuery('#messages').append(li);
+  // var li = jQuery('<li></li>');
+  // var a = jQuery('<a target="_blank">My Current Location</a>');
+
+  // li.text(`${message.sender} ${formattedt}: `);
+  // a.attr('href', message.url);
+
+  // li.append(a);
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (event) {
   event.preventDefault();
   var input = jQuery('[name=message]');
+  var message = input.val();
+  var sender = username.val();
 
+  if (sender.trim() === '') {
+    return alert('Please enter a username to join the chat!');
+  }
+
+  if (message.trim() === '') {
+
+    return;
+  }
+  console.log('Sender', sender);
   socket.emit('sendMessage', {
-      sender: jQuery('[name=username]').val(), 
-      text: input.val()
+      sender, 
+      text: message
     }, function () {
-      console.log('acknowledged sent message');
+      // console.log('acknowledged sent message');
       input.val('');
     });
 });
@@ -56,17 +109,19 @@ locationButton.on('click', function () {
     alert('Your browser does not support geolocation!');
     return;
   }
-
-  var uname = jQuery('[name=username]').val();
+  
+  if (username.val().trim() === '') {
+    return alert('Please enter a username to join the chat!');
+  }
 
   locationButton.attr('disabled','disabled');
   locationButton.text('Sending...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
-    console.log(position);
+    // console.log(position);
 
     socket.emit('sendGeolocationMessage',{
-      sender: uname,
+      sender: username.val(),
       location: {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
